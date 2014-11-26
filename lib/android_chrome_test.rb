1495a4ac7640e58e1dfe56715b5b3f7a667fe2fb
Philippe Bernard <philippe.bernard@gmail.com>
2014-11-26 09:26:10 +0100

    Android Chrome tests added

---
+++ lib/android_chrome_test.rb
@@ -0,0 +1,128 @@
+require 'rfg_api_test'
+
+class AndroidChromeTest < RFGAPITest
+  def test_basic
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        favicon_design: {
+          android_chrome: {
+            manifest: {}
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="manifest" href="/android_chrome_manifest.json">
+EOT
+    )
+  end
+  
+  def test_basic_not_in_root
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        files_location: {
+          type: :path,
+          path: "/path/to/icons"
+        },
+        favicon_design: {
+          android_chrome: {
+            master_picture: {
+              type: :inline,
+              content: "#{to_base64('resources/master_1_120x120.png')}"
+            },
+            manifest: {
+              name: 'Sample app',
+              start_url: 'http://example.com/android'
+            }
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="manifest" href="/path/to/icons/android_chrome_manifest.json">
+EOT
+    )
+  end
+  
+  def test_background_and_margin
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        favicon_design: {
+          android_chrome: {
+            picture_aspect: :background_and_margin,
+            background_color: '#abcdef',
+            margin: '10%',
+            manifest: {
+              name: 'An app',
+              orientation: :portrait,
+              display: :standalone,
+              start_url: 'http://example.com/android'
+            },
+            theme_color: '#469716'
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="manifest" href="/android_chrome_manifest.json">
+<meta name="theme-color" content="#469716">
+EOT
+    )
+  end
+  
+  def test_shadow
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        favicon_design: {
+          android_chrome: {
+            picture_aspect: :shadow,
+            manifest: {},
+            theme_color: '#fedcba'
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="manifest" href="/android_chrome_manifest.json">
+<meta name="theme-color" content="#fedcba">
+EOT
+    )
+  end
+  
+  def test_no_change
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        favicon_design: {
+          android_chrome: {
+            picture_aspect: :no_change,
+            manifest: {}
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="manifest" href="/android_chrome_manifest.json">
+EOT
+    )
+  end
+end
