1b2b92c84245a0efa7421304f972ef24f64335a6
Philippe Bernard <philippe.bernard@gmail.com>
2014-12-09 09:23:00 +0100

    Desktop icons also generated in global test cases

--- lib/global_test.rb
+++ lib/global_test.rb
@@ -10,6 +10,7 @@ class GlobalTest < RFGAPITest
           url: "http://realfavicongenerator.net/demo_favicon.png"
         },
         favicon_design: {
+          desktop_browser: {},
           ios: {
             picture_aspect: :background_and_margin,
             background_color: '#ff0099',
@@ -71,6 +72,11 @@ class GlobalTest < RFGAPITest
 <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 <meta name="msapplication-TileColor" content="#789abc">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <link rel="manifest" href="/android_chrome_manifest.json">
@@ -91,6 +97,7 @@ EOT
           url: "http://realfavicongenerator.net/demo_favicon.png"
         },
         favicon_design: {
+          desktop_browser: {},
           ios: {
             picture_aspect: :background_and_margin,
             background_color: '#ff0099',
@@ -156,6 +163,11 @@ EOT
 <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 <meta name="msapplication-TileColor" content="#789abc">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <link rel="manifest" href="/android_chrome_manifest.json">
