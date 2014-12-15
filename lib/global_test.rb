b881a7f6583c0c1e7d43959480d2d0d333c1c4fb
Philippe Bernard <philippe.bernard@gmail.com>
2014-12-15 23:30:56 +0100

    Verification test added for Grunt plugin issue12 (https://github.com/RealFaviconGenerator/grunt-real-favicon/issues/12)

--- lib/global_test.rb
+++ lib/global_test.rb
@@ -265,4 +265,64 @@ EOT
 EOT
     )
   end
+  
+  # See https://github.com/RealFaviconGenerator/grunt-real-favicon/issues/12
+  def test_issue_12
+    favicon_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :inline,
+          content: "#{to_base64('resources/master_svg_issue_12.svg')}"
+        },
+        favicon_design: {
+          desktop_browser: {},
+          ios: {
+               picture_aspect: 'background_and_margin',
+               background_color: '#000000',
+               margin: 7,
+           },
+           android_chrome: {
+               picture_aspect: 'background_and_margin',
+               background_color: '#000000',
+               margin: 7,
+               manifest: {
+                   name: 'Slevomat.cz',
+                   display: 'browser'
+               },
+           },
+           coast: {
+               picture_aspect: 'background_and_margin',
+               background_color: '#000000',
+               margin: 7,
+           },
+           windows: {
+               picture_aspect: 'no_change',
+               background_color: '#000000',
+           }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<link rel="manifest" href="/android_chrome_manifest.json">
+<link rel="icon" type="image/png" href="/coast-228x228.png" sizes="228x228">
+EOT
+    )
+  end
+  
 end
