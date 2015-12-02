fbab32438c919c0dc74fea4d0a6860ffa280d3fd
Philippe Bernard <philippe.bernard@gmail.com>
2015-12-02 12:53:13 +0100

    Update for v0.12

--- lib/rfg_api_test.rb
+++ lib/rfg_api_test.rb
@@ -8,23 +8,23 @@ require 'base64'
 
 class RFGAPITest < MiniTest::Unit::TestCase
   include Magick
-  
+
   def setup
     FileUtils.rm_rf 'observed_files'
   end
 
   def favicon_generation(request, expected_file_dir, expected_html, is_regexpr = false)
     response = RestClient.post("https://realfavicongenerator.net/api/favicon", request.to_json, content_type: :json)
-    
+
     response = JSON.parse response.body
-    
+
     assert_equal 'success', response['favicon_generation_result']['result']['status']
     if is_regexpr
       assert_match response['favicon_generation_result']['favicon']['html_code'] + "\n", expected_html
     else
       assert_equal expected_html, response['favicon_generation_result']['favicon']['html_code'] + "\n"
     end
-    
+
     dir = observed_dir_path(1)
     # Download the package
     FileUtils.mkdir_p "#{dir}/package"
@@ -34,7 +34,7 @@ class RFGAPITest < MiniTest::Unit::TestCase
     response['favicon_generation_result']['favicon']['files_urls'].each do |file|
       download_file file, "#{dir}/individual_files"
     end
-    
+
     assert_directory_equal expected_file_dir, "#{dir}/package", "Comparison of #{dir}/package and #{expected_file_dir}"
     assert_directory_equal "#{dir}/individual_files", "#{dir}/package", "Comparison of #{dir}/individual_files and #{dir}/package"
   end
@@ -47,7 +47,7 @@ class RFGAPITest < MiniTest::Unit::TestCase
       assert_equal 400, e.response.code
     end
   end
-  
+
   def assert_directory_equal(expected_dir, observed_dir, msg = nil)
     observed_files = files_list(observed_dir).sort
     assert_equal files_list(expected_dir).sort, observed_files, msg
@@ -55,7 +55,7 @@ class RFGAPITest < MiniTest::Unit::TestCase
       assert_file_equal "#{expected_dir}/#{file}", "#{observed_dir}/#{file}"
     end
   end
-  
+
   def assert_file_equal(expected_file, observed_file)
     case File.extname(observed_file)
     when '.ico'
@@ -70,22 +70,23 @@ class RFGAPITest < MiniTest::Unit::TestCase
     when '.png'
       obs = ImageList.new(observed_file).minify
       exp = ImageList.new(expected_file).minify
-      
-      # See https://github.com/RealFaviconGenerator/realfavicongenerator/issues/157
-      assert_equal 8, get_pixel_depth(observed_file), "Pixel depth of #{observed_file}"
-      
+
       assert_equal exp.format, obs.format
       assert_equal [exp.columns, exp.rows], [obs.columns, obs.rows], "Dimensions of #{expected_file} vs #{observed_file}"
       if (exp.columns < 500) and (exp.rows < 500)
         # Large pictures cause the test to crash...
-        assert_equal get_image_pixel(exp), get_image_pixel(obs)
+
+        # See https://github.com/RealFaviconGenerator/realfavicongenerator/issues/157
+        assert_equal 8, get_pixel_depth(observed_file), "Pixel depth of #{observed_file}"
+
+        assert_equal get_image_pixel(exp), get_image_pixel(obs), "Content of #{observed_file}"
       end
       assert_file_size(expected_file, observed_file)
     else
       assert_equal File.read(expected_file), File.read(observed_file)
     end
   end
-  
+
   def assert_file_size(expected_file, observed_file, threshold = 0.25)
     es = File.size(expected_file)
     os = File.size(observed_file)
@@ -94,10 +95,10 @@ class RFGAPITest < MiniTest::Unit::TestCase
     assert ((low < os) and (os < high)),
       "#{expected_file} is #{os} bytes large. Its size should be between #{low} and #{high}"
   end
-  
+
   def extract_ico(ico_file, output_dir)
     system("icotool -x -o #{output_dir} #{ico_file} 2>/dev/null")
-    
+
     Dir.foreach(output_dir) do |item|
       next if item == '.' or item == '..'
       if item =~ /.*(\d+x\d+)x\d+\.png/
@@ -105,11 +106,11 @@ class RFGAPITest < MiniTest::Unit::TestCase
       end
     end
   end
-  
+
   def get_image_pixel(image)
     image.get_pixels(0, 0, image.columns, image.rows).map {|p| p.to_color}
   end
-  
+
   def files_list(directory, base_dir = nil)
     list = []
     Dir.foreach(directory) do |item|
@@ -122,27 +123,27 @@ class RFGAPITest < MiniTest::Unit::TestCase
     end
     list
   end
-  
+
   def download_package(package_url, output_dir)
     file = Tempfile.new('fav_package')
     file.close
     download_file package_url, file.path
     extract_zip file.path(), output_dir
   end
-  
+
   def download_file(url, local_path)
     if File.directory?(local_path)
       uri = URI.parse(url)
       local_path += '/' + File.basename(uri.path)
     end
-    
+
     File.open(local_path, "wb") do |saved_file|
       open(url, "rb") do |read_file|
         saved_file.write(read_file.read)
       end
     end
   end
-  
+
   def extract_zip(zip_path, output_dir)
     Zip::ZipFile.open zip_path do |zip_file|
       zip_file.each do |f|
@@ -152,7 +153,7 @@ class RFGAPITest < MiniTest::Unit::TestCase
       end
     end
   end
-  
+
   def observed_dir_path(level = 0)
     # eg. "test_some_stuff"
     method = (caller[level] =~ /`([^']*)'/ and $1)
@@ -161,7 +162,7 @@ class RFGAPITest < MiniTest::Unit::TestCase
     # eg. observed_files/some_test/test_some_stuff
     dir = 'observed_files/' + file + '/' + method
   end
-  
+
   def expected_dir_path
     # eg. "test_some_stuff"
     method = (caller[0] =~ /`([^']*)'/ and $1)
@@ -170,11 +171,11 @@ class RFGAPITest < MiniTest::Unit::TestCase
     # eg. expected_files/some_test/test_some_stuff
     dir = 'expected_files/' + file + '/' + method
   end
-  
+
   def to_base64(file)
     Base64.strict_encode64 File.read(file)
   end
-  
+
   def get_pixel_depth(image)
     out = `identify #{image}`
     return (out =~ /\s+(\d+)-bit\s+/) ? $1.to_i : nil
