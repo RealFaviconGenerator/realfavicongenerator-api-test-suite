378cbd65c011c2db466bf78d2fc89842bc55813d
Philippe Bernard <philippe.bernard@gmail.com>
2016-09-23 10:53:15 +0200

    'All fields' test

--- lib/social/open_graph_test.rb
+++ lib/social/open_graph_test.rb
@@ -18,4 +18,48 @@ class OpenGraphTest < SocialApiTest
 EOT
     )
   end
+
+  def test_all_fields
+    social_generation({
+      favicon_generation: {
+        api_key: "87d5cd739b05c00416c4a19cd14a8bb5632ea563",
+        master_picture: {
+          type: :url,
+          url: "http://realfavicongenerator.net/demo_favicon.png"
+        },
+        files_location: {
+          type: :path,
+          path: "/path/to/icons"
+        },
+        favicon_design: {
+          facebook_open_graph: {
+            title: "The title",
+            description: "The description",
+            url: "http://www.example.com/my_page",
+            facebook_app_id: "456abc",
+            object_type: "article",
+            site_name: "Sample Site",
+            locale: "en-US",
+            image: {
+              left: 50,
+              top: 50,
+              scale: 0.8,
+              ratio: :square
+            }
+          }
+        }
+      }
+    }, expected_dir_path, <<EOT
+<meta property="og:image" content="/path/to/icons/og-image.jpg">
+<meta property="og:app_id" content="456abc">
+<meta property="og:site_name" content="Sample Site">
+<meta property="og:description" content="The description">
+<meta property="og:title" content="The title">
+<meta property="og:type" content="article">
+<meta property="og:locale" content="en-US">
+<meta property="og:url" content="http://www.example.com/my_page">
+EOT
+    )
+  end
+
 end
--- lib/social/social_api_test.rb
+++ lib/social/social_api_test.rb
@@ -2,6 +2,15 @@ require 'rfg_api_test'
 
 class SocialApiTest < RFGAPITest
 
+  def observed_dir_path(level = 0)
+    # eg. "test_some_stuff"
+    method = (caller[level] =~ /`([^']*)'/ and $1)
+    # eg. "some_test"
+    file = (caller[level] =~ /([^\/:]*).rb:/ and $1)
+    # eg. observed_files/some_test/test_some_stuff
+    dir = 'observed_files/social/' + file + '/' + method
+  end
+
   def expected_dir_path
     # eg. "test_some_stuff"
     method = (caller[0] =~ /`([^']*)'/ and $1)
